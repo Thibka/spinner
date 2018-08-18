e597a0144c909cdd7231384448acc0f18afef485
Thibka <thibaut.foussard@gmail.com>
2018-08-18 21:12:43 +0200

    Init commit

---
+++ README.md
@@ -0,0 +1,25 @@
+Description
+==================
+Sometimes CSS rotation can have unconsistent results depending on the browser, the type and amplitude of rotation, and the technique (transition or animation). 
+For instance, a browser might choose to take the shortest path between to angles. In such a scenario, a +350deg rotation would become a -10deg rotation.
+This class is intended to provide a consistent, JS based, rotation animation.
+
+[Demo](https://jsfiddle.net/Wonderbanners/bc2xdsfq/11/).
+
+
+
+Usage
+==================
+
+```javascript
+var spinner = new Spinner({
+    id: "myElement",
+    axis: "z",
+    start: 0,
+    end: 360,
+    duration: 1000,
+    easing: function() { ... } // optionally, add easing equation. See [Robert Penners equations](https://forum.kirupa.com/t/robert-penners-easing-equations-in-pure-js-no-jquery/330985).
+});
+	
+spinner.spin();
+```
---
+++ Spinner.js
@@ -0,0 +1,48 @@
+// Spinner v. 1.03
+(function(window, document, undefined){
+    var getTime = Date.now || function() {return new Date().getTime();};
+
+    function Spinner(params) {
+        this.element  = document.getElementById(params.id);
+        this.start = params.start;
+        this.end = params.end;
+        this.diff = (this.end > this.start) ? this.end - this.start : -(this.start-this.end);
+        this.duration = params.duration;
+        this.easing = params.easing;
+        this.clock = 0,
+        this.easing =  params.easing || function (currentIteration, startValue, changeInValue, totalIterations) {
+            if ((currentIteration /= totalIterations / 2) < 1) {
+                return changeInValue / 2 * currentIteration * currentIteration + startValue;
+            }
+            return -changeInValue / 2 * ((--currentIteration) * (currentIteration - 2) - 1) + startValue;
+        };
+        
+        switch(params.axis) {
+            case "x": this.axis = "1,0,0"; break;
+            case "y": this.axis = "0,1,0"; break;
+            default : this.axis = "0,0,1";
+        }
+    }
+
+    Spinner.prototype.spin = function() {
+        this.startTime = getTime();
+        this.spinAnimation();
+    }
+
+    Spinner.prototype.spinAnimation = function() {             
+        this.clock = getTime() - this.startTime;
+        
+        currentRotation = this.easing(this.clock, this.start, this.diff, this.duration);
+        this.element.style.webkitTransform = this.element.style.transform = "rotate3d(" + this.axis + "," + currentRotation + "deg)";
+        
+        console.log("rotate3d(" + this.axis + "," + currentRotation + "deg)");
+        if (this.clock < this.duration) requestAnimationFrame(this.spinAnimation.bind(this));
+        else {
+            this.element.style["-webkit-transform"] = "rotate3d(" + this.axis + "," + this.end + "deg)";
+            this.element.style.transform = "rotate3d(" + this.axis + "," + this.end + "deg)";
+        }
+    }
+
+    window.Spinner = Spinner;
+
+})(window, document);
